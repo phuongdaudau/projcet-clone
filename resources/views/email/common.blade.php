0e8c99a646fddc49291b161731335224b2f526c9
PhuongPv <phong@gmail.com>
2022-07-21 01:04:00 +0700

    reset password comon

---
+++ resources/views/email/common.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Reset Password</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    <a href="{{ $details['url'] }}"> Reset Password </a>
+    <p>Thank you</p>
+</body>
+</html>
